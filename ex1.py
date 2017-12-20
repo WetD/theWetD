a3567dd95f418e40b6ead52e207b9209ba437099
WetD <34669849+WetD@users.noreply.github.com>
2017-12-20 12:21:17 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,16 @@
+#3和2的不同print
+ #2.X: print "The answer is", 2*2 
+ #3.X: print("The answer is", 2*2) 
+ #  2.X: print                                 # 输出新行 
+ #  3.X: print()                               # 输出新行 
+print ("Hello World!")
+#代表注释
+print ("Hello Again")
+print ("I Like Typing This.")
+print ('Yay! Printing.')
+print ("I'd Much Rather You 'Not'.")
+print ('I "Said" Do Not Touch This.')
+#多打一行就是print一个空白值
+print ( )
+print ("你好啊~")
+
